fd68ec020253bf9abd4a4d59a330ffff013c7788
Tainaortez <102428677+Tainaortez@users.noreply.github.com>
2023-05-27 16:22:04 -0600

    Add files via upload

---
+++ routes/estudiantes.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+const estudiantes = require('../services/estudiantes');
+
+/* GET estudiantes. */
+router.get('/', async function(req, res, next) {
+  try {
+    res.json(await estudiantes.getMultiple(req.query.page));
+  } catch (err) {
+    console.error(`Error while getting estudiantes `, err.message);
+    next(err);
+  }
+});
+
+/* POST estudiantes */
+router.post('/', async function(req, res, next) {
+    try {
+      res.json(await estudiantes.create(req.body));
+    } catch (err) {
+      console.error(`Error while creating estudiantes`, err.message);
+      next(err);
+    }
+  });
+
+  /* PUT estudiantes */
+router.put('/:id', async function(req, res, next) {
+    try {
+      res.json(await estudiantes.update(req.params.id, req.body));
+    } catch (err) {
+      console.error(`Error while updating estudiantes`, err.message);
+      next(err);
+    }
+  });
+
+/* DELETE estudiantes */
+router.delete('/:id', async function(req, res, next) {
+    try {
+      res.json(await estudiante.remove(req.params.id));
+    } catch (err) {
+      console.error(`Error while deleting estudiante`, err.message);
+      next(err);
+    }
+  });
+
+module.exports = router;
